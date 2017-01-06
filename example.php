cb8e42752914b2422053d6aee62eb1610b1a28cb
Larisa <kosmetichka@mail.ru>
2017-01-06 22:53:59 +0300

    Левое меню и попытка добавления услуг. Сырая версия)

--- example.php
+++ example.php
@@ -1,96 +1,7 @@
-<!DOCTYPE html>
-<html lang="ru">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Прайм-Стоун - столешницы и подоконники из искусственного камня</title>
-
-    <!-- Bootstrap -->
-     <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-
-  <header class="navbar navbar-default navbar-fixed-top">
-  <div class="container-fluid">
-    <div id="logo"><a href="prime-stone.ru"><img src="images/log.gif"></a></div>
-    <div id="desc"> <p><b>Производство изделий <br>из искусственного камня</b></p></div>
-    <div id="adress"><p> Время работы: <br><b>Пн-Пт - 9<sup>00</sup> - 18<sup>00</sup></b><br><span
-    <font color="#00b300"><a href="#map">посмотреть на карте</a></font></span></p></div>
-        <div id="right-header-block">
-          <div class="mail"><p><img src="images/message.jpg"><a href="mailto:info@prime-stone.ru" target="_blank">info@prime-stone.ru</a></p>
-          </div>
-          <div class="telefon-header"><p><b>+7(495)</b><span><font size="4" ><b>227 49 19</b></font></span></p></div>
-  </div>
-
-</header>
-<nav class="navbar navbar-default navbar-fixed-top" id="menu">
-  <div class="container-fluid">
-    <div class="menu"><ul class="nav navbar-nav">
-        <li class="active"><a href="#">Производство<span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Акции</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Продукция <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Столешницы для кухни</a></li>
-            <li><a href="#">Столешницы для ванной комнаты</a></li>
-            <li><a href="#">Подоконники</a></li>
-            <li><a href="#">Барные стойки</a></li>
-            <li><a href="#">Ресепшн</a></li>
-            <li><a href="#">Столешницы для санузлов</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Мойки</a></li>
-            <li><a href="#">Раковины</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Изделия для лестницы</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Наши работы</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Акриловый камень <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Samsung Staron</a></li>
-            <li><a href="#">LG Hi Macs</a></li>
-            <li><a href="#">Corian</a></li>
-            <li><a href="#">Hanex</a></li>
-            <li><a href="#">Tristone</a></li>
-            <li><a href="#">Grandex</a></li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Кварцевый камень <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Samsung Radianz</a></li>
-            <li><a href="#">Caesarstone</a></li>
-            <li><a href="#">Cambria</a></li>
-            <li><a href="#">Plazastone</a></li>
-            <li><a href="#">Scorino</a></li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Еще<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Кромки и бортики для столешниц</a></li>
-            <li><a href="#">Стеновые панели</a></li>
-            <li><a href="#">Накладки для ванных</a></li>
-            <li><a href="#">Тумбы для ванных комнат</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<nav id="left-menu">Левое меню</nav>
+<?php
+$item=1;
+include 'blocks/head.php';
+?>
 <section id="slider">
   <div class="container-fluid">
   <div id="carousel" class="carousel slide">
@@ -99,28 +10,84 @@
    <li class="active" data-target="#carousel" data-slide-to="0"></li>
    <li data-target="#carousel" data-slide-to="1"></li>
    <li data-target="#carousel" data-slide-to="2"></li>
+   <li data-target="#carousel" data-slide-to="3"></li>
+   <li data-target="#carousel" data-slide-to="4"></li>
+   <li data-target="#carousel" data-slide-to="5"></li>
+   <li data-target="#carousel" data-slide-to="6"></li>
+   <li data-target="#carousel" data-slide-to="7"></li>
+   <li data-target="#carousel" data-slide-to="8"></li>
+   <li data-target="#carousel" data-slide-to="9"></li>
    </ol>
    <!--Слайды-->
    <div class="carousel-inner">
      <div class="item active">
        <img src="images/mine1.jpg" alt="">
        <div class="carousel-caption">
-        <h3>Первый слайд</h3>
-        <p>Описание первого слайда</p>
+        <h3>Столешницы</h3>
+        <p> из искусственного камня для кухни</p>
         </div>
      </div>
      <div class="item">
        <img src="images/mine2.jpg" alt="">
        <div class="carousel-caption">
-        <h3>Второй слайд</h3>
-        <p>Описание второго слайда</p>
+        <h3>Изготовление на заказ</h3>
+        <p>в сжатые сроки по индивидуальным замерам</p>
         </div>
      </div>
      <div class="item">
        <img src="images/mine3.jpg" alt="">
        <div class="carousel-caption">
-        <h3>Третий слайд</h3>
-        <p>Описание третьего слайда</p>
+        <h3>Цены от производителя</h3>
+        <p>на изделия из акрилового и кварцевого камня</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine4.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Любые формы и размеры</h3>
+        <p>столешниц, подоконников и других изделий</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine5.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Широкая цветовая гамма</h3>
+        <p>на любой вкус. Более 1000 цветов!</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine6.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Изготовление на заказ</h3>
+        <p>столешниц для санузлов и ванных комнат</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine7.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Кухонные мойки и раковины</h3>
+        <p>в цвет столешницы - без стыков и швов</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine8.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Большой опыт</h3>
+        <p>работы в сфере изготовления изделий из искусственного камня</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine9.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Мы используем только качественный материал</h3>
+        <p>Весь материал сертифицирован</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine10.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Собственное производство</h3>
+        <p>Высокое качество. Гарантия!</p>
         </div>
      </div>
    </div>
@@ -136,13 +103,26 @@
 </section>
 <section id="services">
   <div class="container-fluid">
-    Секция с услугами
+  <h2>Наши изделия и услуги</h2>
+  <div id="block-ramki">
+  <div id="ramka">
+  <div id="img-ramka">
+  <img src="images/eskiz-kuhnya.jpg">Столешницы для кухни</div>
+  <div id="text-ramka">Производим на заказ столешницы для кухни из акрилового и кварцевого камня. Более 1000 цветов!
+  Быстро и качественно! Цены от производителя!
+  Подробнее
+  </div>
+  </div>
+  <div id="ramka">
+  </div>
+  <div id="ramka2">
+   <div id="text-ramka2">Столешницы для кухни
+  </div>
+  </div>
+  </div>
   </div>
 </section>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.js"></script>
-  </body>
-</html>
+</div>
+    <?php
+include 'blocks/footer.php';
+?>
---
+++ example2.php
@@ -0,0 +1,18 @@
+ <?php
+$item=2;
+include 'blocks/head.php';
+?>
+<section id="slider">
+  <div class="container-fluid">
+      Секция с картинками
+  </div>
+</section>
+<section id="services">
+  <div class="container-fluid">
+    Секция с услугами
+  </div>
+</section>
+
+    <?php
+include 'blocks/footer.php';
+?>
