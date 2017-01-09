1d3de3e4e1082afc4e243733b0114ba8a54777e2
Larisa <kosmetichka@mail.ru>
2017-01-09 03:40:20 +0300

    добавление текста в блок с котами

---
+++ blocks/footer.php
@@ -0,0 +1,9 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+    <div class="container-fluid">
+    Подвальчик
+    </div>
+  </body>
+</html>
---
+++ blocks/head.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Прайм-Стоун - столешницы и подоконники из искусственного камня</title>
+
+    <!-- Bootstrap -->
+     <link href="css/bootstrap.css" rel="stylesheet">
+     <link href="css/style.css" rel="stylesheet">
+     <link href="css/font-awesome.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+  <header class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div id="logo"><a href="prime-stone.ru"><img src="images/log.gif"></a></div>
+    <div id="desc"> <p><b>Производство изделий <br>из искусственного камня</b></p></div>
+    <div id="adress"><p> Время работы: <br><b>Пн-Пт - 9<sup>00</sup> - 18<sup>00</sup></b><br><span
+    <font color="#00b300"><a href="#map">посмотреть на карте</a></font></span></p></div>
+        <div id="right-header-block">
+          <div class="mail"><p><img src="images/message.jpg"><a href="mailto:info@prime-stone.ru" target="_blank">info@prime-stone.ru</a></p>
+          </div>
+          <div class="telefon-header"><p><b>+7(495)</b><span><font size="4" ><b>227 49 19</b></font></span></p></div>
+  </div>
+
+</header>
+<nav class="navbar navbar-default navbar-fixed-top" id="menu">
+  <div class="container-fluid">
+    <div class="menu"><ul class="nav navbar-nav">
+        <li<?php if($item==1) {?> class="active"<?php } ?>><a href="example.php">Прайм-Стоун<span class="sr-only">(current)</span></a></li>
+        <li<?php if($item==2) {?> class="active"<?php } ?>><a href="example2.php">Акции</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Продукция <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Столешницы для кухни</a></li>
+            <li><a href="#">Столешницы для ванной комнаты</a></li>
+            <li><a href="#">Подоконники</a></li>
+            <li><a href="#">Барные стойки</a></li>
+            <li><a href="#">Ресепшн</a></li>
+            <li><a href="#">Столешницы для санузлов</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Мойки</a></li>
+            <li><a href="#">Раковины</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Изделия для лестницы</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Наши работы</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Акриловый камень <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Samsung Staron</a></li>
+            <li><a href="#">LG Hi Macs</a></li>
+            <li><a href="#">Corian</a></li>
+            <li><a href="#">Hanex</a></li>
+            <li><a href="#">Tristone</a></li>
+            <li><a href="#">Grandex</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Кварцевый камень <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Samsung Radianz</a></li>
+            <li><a href="#">Caesarstone</a></li>
+            <li><a href="#">Cambria</a></li>
+            <li><a href="#">Plazastone</a></li>
+            <li><a href="#">Scorino</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Еще<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Наше производство</a></li>
+            <li><a href="#">Кромки и бортики для столешниц</a></li>
+            <li><a href="#">Стеновые панели</a></li>
+            <li><a href="#">Накладки для ванных</a></li>
+            <li><a href="#">Тумбы для ванных комнат</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<nav id="left-menu">
+<div class="block">
+<ul class="list-group">
+<br>
+<li><a href="#"><i class="fa fa-book fa-2x" aria-hidden="true" style="color: #ffffff"></i>
+<p style="font-size: 10px; color: #000000">Продукция</p></a></li>
+<li><a href="#"><i class="fa fa fa-picture-o fa-2x" aria-hidden="true" style="color: #ffffff"></i>
+<p style="font-size: 10px; color: #000000">Цвета камня</p></a></li>
+<li><a href="#"><i class="fa fa-balance-scale fa-2x" aria-hidden="true" style="color: #ffffff"></i>
+<p style="font-size: 10px; color: #000000">Для кухни</p></a></li>
+<li><a href="#"><i class="fa fa-bath fa-2x" aria-hidden="true" style="color: #ffffff"></i>
+<p style="font-size: 10px; color: #000000">Для ванной</p></a></li>
+<li><a href="#"><i class="fa fa-window-maximize fa-rotate-180 fa-2x" aria-hidden="true" style="color: #ffffff"></i>
+<p style="font-size: 10px; color: #000000">Подоконники</p></a></li>
+<li><a href="#"><i class="fa fa-file-image-o fa-2x" aria-hidden="true" style="color: #ffffff"></i>
+<p style="font-size: 10px; color: #000000">Фото</p></a></li>
+<li><a href="#"><i class="fa fa-calculator fa-2x" aria-hidden="true" style="color: #ffffff"></i>
+<p style="font-size: 10px; color: #000000">Цены</p></a></li>
+<li><a href="#"><i class="fa  fa-envelope-o fa-2x" aria-hidden="true" style="color: #ffffff"></i>
+<p style="font-size: 10px; color: #000000">Контакты</p></a></li>
+</ul>
+</div>
+</nav>
