6bcb5d0e82cfeba96cae784af729155b59a6d0c1
Vasilii Chugunov <naukomania@gmail.com>
2017-01-13 12:39:42 +0300

    Новый файл example3.php копия example.php

---
+++ example3.php
@@ -0,0 +1,189 @@
+<?php
+$item=1;
+include 'blocks/head.php';
+?>
+<section id="slider">
+  <div class="container-fluid">
+   <div id="imgtopslide">
+  </div>
+  <div id="carousel" class="carousel slide">
+  <!--Индикаторы слайдов-->
+   <ol class="carousel-indicators">
+   <li class="active" data-target="#carousel" data-slide-to="0"></li>
+   <li data-target="#carousel" data-slide-to="1"></li>
+   <li data-target="#carousel" data-slide-to="2"></li>
+   <li data-target="#carousel" data-slide-to="3"></li>
+   <li data-target="#carousel" data-slide-to="4"></li>
+   <li data-target="#carousel" data-slide-to="5"></li>
+   <li data-target="#carousel" data-slide-to="6"></li>
+   <li data-target="#carousel" data-slide-to="7"></li>
+   <li data-target="#carousel" data-slide-to="8"></li>
+   <li data-target="#carousel" data-slide-to="9"></li>
+   </ol>
+   <!--Слайды-->
+   <div class="carousel-inner">
+     <div class="item active">
+       <img src="images/mine1.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Столешницы</h3>
+        <p> из искусственного камня для кухни</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine2.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Изготовление на заказ</h3>
+        <p>в сжатые сроки по индивидуальным замерам</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine3.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Цены от производителя</h3>
+        <p>на изделия из акрилового и кварцевого камня</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine4.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Любые формы и размеры</h3>
+        <p>столешниц, подоконников и других изделий</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine5.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Широкая цветовая гамма</h3>
+        <p>на любой вкус. Более 1000 цветов!</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine6.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Изготовление на заказ</h3>
+        <p>столешниц для санузлов и ванных комнат</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine7.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Кухонные мойки и раковины</h3>
+        <p>в цвет столешницы - без стыков и швов</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine8.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Большой опыт</h3>
+        <p>работы в сфере изготовления изделий из искусственного камня</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine9.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Мы используем только качественный материал</h3>
+        <p>Весь материал сертифицирован</p>
+        </div>
+     </div>
+     <div class="item">
+       <img src="images/mine10.jpg" alt="">
+       <div class="carousel-caption">
+        <h3>Собственное производство</h3>
+        <p>Высокое качество. Гарантия!</p>
+        </div>
+     </div>
+   </div>
+   <!--Стрелки переключения слайдов-->
+   <a href="#carousel" class="left carousel-control" data-slide="prev">
+     <span class="glyphicon glyphicon-chevron-left"></span>
+   </a>
+   <a href="#carousel" class="right carousel-control" data-slide="next">
+     <span class="glyphicon glyphicon-chevron-right"></span>
+   </a>
+   <div id="imgtopslide">
+  </div>
+  </div>
+</section>
+<section id="services">
+  <div class="container-fluid">
+   <h4><font color="212121"> Прайм-Стоун предлагает изготовить для вас</font></h4>
+    <div id="block-ramki">
+     <div id="ramka"> 
+      <div id="text-ramka">Столешницы для кухни</div>
+      <div id="text-ramka2">Мы изготавливаем кухонные столешницы из искусственного акрилового и кварцевого камня. Оптимальными вариантами для изготовления столешниц >>>></div>
+          </div>
+    <div id="ramka2"> 
+      <div id="text-ramka">&nbsp &nbsp Столешницы для ванной</div>
+    </div>
+    <div id="ramka3"> 
+      <div id="text-ramka">&nbsp &nbsp &nbsp &nbsp &nbsp Подоконники</div>
+    </div>
+    </div>
+    <img src="images/line2.png">
+  <div id="block-ramki">
+     <div id="ramka4"> 
+      <div id="text-ramka">&nbsp &nbsp &nbsp &nbsp &nbspБарные стойки</div>
+    </div>
+    <div id="ramka5"> 
+      <div id="text-ramka">&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Ресепшн</div>
+    </div>
+    <div id="ramka6"> 
+      <div id="text-ramka">Столешницы для санузлов</div>
+    </div>
+  </div>
+   <img src="images/line2.png">
+  </div class="container-fluid">
+  <div id="razdel-content">
+  <h4>Палитра камней, цвета и текстуры</h4>
+  Акриловый камень
+  </div>
+  </div>
+  <div id="katalog-kamnya">
+  <div class="tabbable"> <!-- Only required for left/right tabs -->
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#tab1" data-toggle="tab">Samsung Staron</a></li>
+    <li><a href="#tab2" data-toggle="tab">LG Hi Macs</a></li>
+    <li><a href="#tab3" data-toggle="tab">Corian</a></li>
+    <li><a href="#tab4" data-toggle="tab">Hanex</a></li>
+    <li><a href="#tab5" data-toggle="tab">Tristone</a></li>
+    <li><a href="#tab6" data-toggle="tab">Grandex</a></li>
+    <li><a href="#tab7" data-toggle="tab">Kerrock</a></li>
+  </ul>
+
+  <div class="tab-content">
+    <div class="tab-pane active" id="tab1">
+      <div id="a123">
+      <img src="images/formstone1.png">
+      </div>
+      <div id="a123">
+      <img src="images/formstone2.png">
+      </div>
+      <div id="a123">
+      <img src="images/formstone3.png">
+      </div>
+      <div id="a123">
+      <img src="images/formstone4.png">
+      </div>
+      <div id="a123">
+      <img src="images/formstone5.png">
+      </div>
+      <div id="a123">
+      <img src="images/formstone6.png">
+      </div>
+      <div id="a123">
+      <img src="images/formstone7.png">
+      </div>
+    </div>
+    <div class="tab-pane" id="tab2">
+      <p>Привет, я в Разделе 2.</p>
+    </div>
+    <div class="tab-pane" id="tab3">
+      <p>Привет, я в Разделе 3.</p>
+    </div>
+  </div>
+</div>
+</section>
+
+    <?php
+include 'blocks/footer.php';
+?>
