151c9eb20dd6f303e2bf91254b4d31c9cab4fcd8
Larisa <kosmetichka@mail.ru>
2017-02-03 13:02:58 +0300

    Последние изменения лефт меню

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@
   $(document).ready(function(){
  	// при клике на кота и на левое меню
  	$('#services .item, #left-menu .item').on('click',function(){
+ 		$(this).addClass('active');
  		var target = $(this).attr('data-target');
  		$('#' + target  +', #background-wrapper' ).fadeIn('slow'); // показываем лайтбокс
 	});
   // закрываем лайтбокс при клике на крестик
  	$('.closeBox').on('click',function(){
 		$(this).parents('.lightbox').fadeOut();
+		$(' #left-menu .item').removeClass('active');
 			$(' #background-wrapper' ).fadeOut();
 	});
 	$('.nextPage').on('click',function(){
