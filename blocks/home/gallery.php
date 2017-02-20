fa87d305e88aa9e5d7bced6423b6cfdefdae4d2d
Larisa <kosmetichka@mail.ru>
2017-02-20 12:16:23 +0300

    Последнее

--- blocks/home/gallery.php
+++ blocks/home/gallery.php
@@ -25,58 +25,11 @@
 </div>
        </div>
         <div class="tab-pane" id="tab3">
-           <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
-        	<br>
-        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_1.JPG"> 
-          <img class="img-responsive" src="/images/gallery/podok/gall_podok_1.JPG"> 
-        </a> 
-        </div>
-        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
-        	<br>
-        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_2.JPG"> 
-          <img class="img-responsive" src="/images/gallery/podok/gall_podok_2.JPG"> 
-        </a> 
-        </div>
-        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
-        	<br>
-        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_3.JPG"> 
-          <img class="img-responsive" src="/images/gallery/podok/gall_podok_3.JPG"> 
-        </a> 
-        </div>
-        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
-        	<br>
-        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_4.JPG"> 
-          <img class="img-responsive" src="/images/gallery/podok/gall_podok_4.JPG"> 
-        </a> 
-        </div>
-        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
-        	<br>
-        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_5.JPG"> 
-          <img class="img-responsive" src="/images/gallery/podok/gall_podok_5.JPG"> 
-        </a> 
-        </div>
-        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
-        	<br>
-        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_6.JPG"> 
-          <img class="img-responsive" src="/images/gallery/podok/gall_podok_6.JPG"> 
-        </a> 
-        </div>
-        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
-        	<br>
-        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_7.JPG"> 
-          <img class="img-responsive" src="/images/gallery/podok/gall_podok_7.JPG"> 
-        </a> 
-        </div>
-        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
-        	<br>
-        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_8.JPG"> 
-          <img class="img-responsive" src="/images/gallery/podok/gall_podok_8.JPG"> 
-        </a> 
-        </div>
-        <br>
-        <br>
-          <button type="button"class="btn btn-success"><a href="#">Смотреть фото еще фото  подоконники из искусственного камня</a></button>
-        </div>
+          <div class="row ajax-foto-wrapper-podok">
+            <? include 'blocks/home/gallery/podok/page1.php'; ?>
+
+</div>
+       </div>
           <div class="tab-pane" id="tab4">
            <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
         	<br>
---
+++ blocks/home/gallery/podok/page1.php
@@ -0,0 +1,52 @@
+ <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
+            <br>
+        <a class="fancyimage" rel="group" title="Эркерный подоконник из акрилового камня" href="/images/gallery/podok/gall_podok_1.JPG"> 
+          <img class="img-responsive" src="/images/gallery/podok/gall_podok_1.JPG"> Эркерный подоконник
+        </a> 
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
+            <br>
+        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_2.JPG"> 
+          <img class="img-responsive" src="/images/gallery/podok/gall_podok_2.JPG"> 
+        </a> 
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
+            <br>
+        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_3.JPG"> 
+          <img class="img-responsive" src="/images/gallery/podok/gall_podok_3.JPG"> 
+        </a> 
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
+            <br>
+        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_4.JPG"> 
+          <img class="img-responsive" src="/images/gallery/podok/gall_podok_4.JPG"> 
+        </a> 
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
+            <br>
+        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_5.JPG"> 
+          <img class="img-responsive" src="/images/gallery/podok/gall_podok_5.JPG"> 
+        </a> 
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
+            <br>
+        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_6.JPG"> 
+          <img class="img-responsive" src="/images/gallery/podok/gall_podok_6.JPG"> 
+        </a> 
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
+            <br>
+        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_7.JPG"> 
+          <img class="img-responsive" src="/images/gallery/podok/gall_podok_7.JPG"> 
+        </a> 
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6 thumb"> 
+            <br>
+        <a class="fancyimage" rel="group" href="/images/gallery/podok/gall_podok_8.JPG"> 
+          <img class="img-responsive" src="/images/gallery/podok/gall_podok_8.JPG"> 
+        </a> 
+        </div>
+        <br>
+        <br>
+          
+        </div>
