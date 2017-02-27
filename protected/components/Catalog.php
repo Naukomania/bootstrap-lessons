89fc563468cb42fc1ea1c404455a1280aaea65bc
Larisa <kosmetichka@mail.ru>
2017-02-27 18:37:12 +0300

    Урок

--- protected/components/Catalog.php
+++ protected/components/Catalog.php
@@ -10,17 +10,55 @@ class Catalog {
             'keywords' => 'Самсунг старон, samsung staron, staron, старон',
              'logoSrc'=>'/images/logostaron.jpg',
             'description' => 'Каталог цветов искусственного акрилового камня Samsung Staron (самсунг старон).',
+              'descriptionText' => 'Искусственный камень Staron пользуется огромной популярностью во всем мире. Он обладает превосходной прочностью и имеет чрезвычайно богатую палитру, представленную многообразием цветов, оттенков и текстур.
+                Профессиональные дизайнеры интерьеров предпочитают работать именно с изделиями из акрилового камня Staron, поскольку он поддается термоформингу (детали с изогнутыми элементами изготавливаются из плоского листка материала) и характеризуется богатством цветовой палитры Staron. Возможность разработки конструкции любой сложности без видимых швов также является несомненным преимуществом использования искусственного камня Самсунг Старон.',
+                 'descriptionCollName' => 'Искусственный камень Samsung Staron',
+
+ 'descriptionCollEx' => 'Искусственный камень Staron пользуется огромной популярностью во всем мире. Он обладает превосходной прочностью и имеет чрезвычайно богатую палитру, представленную многообразием цветов, оттенков и текстур.
+                Профессиональные дизайнеры интерьеров предпочитают работать именно с изделиями из акрилового камня Staron, поскольку он поддается термоформингу (детали с изогнутыми элементами изготавливаются из плоского листка материала) и характеризуется богатством цветовой палитры Staron. Возможность разработки конструкции любой сложности без видимых швов также является несомненным преимуществом использования искусственного камня Самсунг Старон.
+                Все, кто делает выбор в пользу красивого и экологически чистого искусственного камня Samsung Staron, остаются довольны результатом. Широкое распространение материал получил благодаря своим следующим свойствам:
+                Бесшовное соединение искусственного камня Самсунг по цене доступной и приемлемой позволяет производить разнообразные по форме поверхности.
+                Непористая структура акрилового камня Самсунг Старон гарантирует простой повседневный уход и долговечность конструкций.
+                Благодаря кварцевым частицам искусственный камень Старон обладает природной красотой и прочностью. Его эстетика и прекрасные потребительские свойства расширяют дизайнерские возможности при разработке богатых и изысканных интерьеров. Искусственный камень Самсунг используется для изготовления кухонных столешниц Samsung Staron и подоконников, барных стоек, отделки ванных комнат, торгового оборудования и т. д. Не менее привлекательны и мойки Старон, способные привнести в помещение ноту изысканности.',
+                'breadcrumb1'=>'Solid Color',
+                'breadcrumb2'=>'Super Color',
+                'breadcrumb3'=>'Sanded',
+                'breadcrumb4'=>'Aspen',
+                'breadcrumb5'=>'Metallic',
+                'breadcrumb6'=>'Pebble',
+                 'breadcrumb7'=>'Quarry',
+                'breadcrumb8'=>'Tempest',
+                'breadcrumb9'=>'Supreme',
+
         ];
+
+
         $items['staron']['collection'] = [
-            'title' => 'Коллекция Aspen',
+            'title' => 'Коллекция Pebble',
             'items' => [
                 [
-                    'title' => 'Aspen Brown - AB632',
-                    'src' => '/samsung_staron/images/katalog/staron_ab632_aspen_brown.jpg',
+                    'title' => 'Pebble Aqua - PA860',
+                    'src' => '/samsung_staron/images/katalog/staron_pa860_pebble_aqua.jpg',
                 ],
                 [
-                    'title' => 'Aspen Brown - AB632',
-                    'src' => '/samsung_staron/images/katalog/staron_ab632_aspen_brown.jpg',
+                    'title' => 'Pebble Terrain- PT857',
+                    'src' => '/samsung_staron/images/katalog/staron_pb857_pebble_terrain.jpg',
+                ],
+                [
+                    'title' => 'Pebble Blue PB-870' ,
+                    'src' => '/samsung_staron/images/katalog/staron_pb870_pebble_blue.jpg',
+                ],
+                [
+                    'title' => 'Pebble Cooper - PC851' ,
+                    'src' => '/samsung_staron/images/katalog/staron_pc851_pebble_copper.jpg',
+                ],
+                 [
+                    'title' => 'Pebble Confetti - PC880' ,
+                    'src' => '/samsung_staron/images/katalog/staron_pc880_pebble_confetti.jpg',
+                ],
+                  [
+                    'title' => 'Pebble Ebony - PE814' ,
+                    'src' => '/samsung_staron/images/katalog/staron_pe814_pebble_ebony.jpg',
                 ],
             ],
         ];
@@ -31,17 +69,50 @@ class Catalog {
     'keywords' => 'LG Hi macs,  хай макс',
      'logoSrc'=>'/images/logo-lg-himacs.png',
     'description' => 'Каталог цветов искусственного акрилового камня LG Hi Macs (элджи хай макс).',
-];
+      'descriptionText' => 'Искусственный камень LG пользуется огромной популярностью во всем мире. Он обладает превосходной прочностью и имеет чрезвычайно богатую палитру, представленную многообразием цветов, оттенков и текстур.
+                Профессиональные дизайнеры интерьеров предпочитают работать именно с изделиями из акрилового камня Staron, поскольку он под   LG LG LG LG LG LG LG  LG LG LG LG LG LG LG  LG LG LG LG LG LG LG  LG LG LG LG LG LG LG.',
+                  'descriptionCollName' => '  Искусственный камень LG Hi Macs',
+                  'descriptionCollEx' => '  Искусственный камень Staron пользуется огромной популярностью во всем мире. Он обладает превосходной прочностью и имеет чрезвычайно богатую палитру, представленную многообразием цветов, оттенков и текстур.
+                Профессиональные дизайнеры интерьеров предпочитают работать именно с изделиями из акрилового камня Staron, поскольку он поддается термоформингу (детали с изогнутыми элементами изготавливаются из плоского листка материала) и характеризуется богатством цветовой палитры Staron. Возможность разработки конструкции любой сложности без видимых швов также является несомненным преимуществом использования искусственного камня Самсунг Старон.
+                Все, кто делает выбор в пользу красивого и экологически чистого искусственного камня Samsung Staron, остаются довольны результатом. Широкое распространение материал получил благодаря своим следующим свойствам:
+                Бесшовное соединение искусственного камня Самсунг по цене доступной и приемлемой позволяет производить разнообразные по форме поверхности.
+                Непористая структура акрилового камня Самсунг Старон гарантирует простой повседневный уход и долговечность конструкций.
+                Благодаря кварцевым частицам искусственный камень Старон обладает природной красотой и прочностью. Его эстетика и прекрасные потребительские свойства расширяют дизайнерские возможности при разработке богатых и изысканных интерьеров. Искусственный камень Самсунг используется для изготовления кухонных столешниц Samsung Staron и подоконников, барных стоек, отделки ванных комнат, торгового оборудования и т. д. Не менее привлекательны и мойки Старон, способные привнести в помещение ноту изысканности.',
+                 'descriptionCollEx' => ' LG LG LG LG LG LG LG LG LG LG LG LG LG LG  LG LG LG LG LG LG LG  LG LG LG LG LG LG LG  LG LG LG LG LG LG LG  LG LG LG LG LG LG LG  LG LG LG LG LG LG LG  LG LG LG LG LG LG LG  LG LG LG LG LG LG LG  LG LG LG LG LG LG LG  LG LG LG LG LG LG LG',
+                 'breadcrumb1'=>'Solid',
+                'breadcrumb2'=>'Sand&Pearl',
+                'breadcrumb3'=>'Quartz&Granit',
+                'breadcrumb4'=>'Lusent',
+                'breadcrumb5'=>'Sparkle&Volcanics',
+                'breadcrumb6'=>'Galaxy',
+                 'breadcrumb7'=>'Marmo',
+                 ];
 $items['lg_hi_macs']['collection'] = [
-    'title' => 'Коллекция Marmo',
+    'title' => 'Коллекция Galaxy',
     'items' => [
         [
-            'title' => 'Aspen Brown - AB632',
-            'src' => '/samsung_staron/images/katalog/staron_ab632_aspen_brown.jpg',
+            'title' => 'Black Hole - T001',
+            'src' => '/lg_hi_macs/images/catalog/lg_hi_macs_galaxy_t001_black_hole.jpg',
         ],
         [
-            'title' => 'Aspen Brown - AB632',
-            'src' => '/samsung_staron/images/katalog/staron_ab632_aspen_brown.jpg',
+            'title' => 'Uranus - T002',
+            'src' => '/lg_hi_macs/images/catalog/lg_hi_macs_galaxy_t002_uranus.jpg',
+        ],
+         [
+            'title' => 'Saturn - T004',
+            'src' => '/lg_hi_macs/images/catalog/lg_hi_macs_galaxy_t004_saturn.jpg',
+        ],
+         [
+            'title' => 'Comet - T006',
+            'src' => '/lg_hi_macs/images/catalog/lg_hi_macs_galaxy_t006_comet.jpg',
+        ],
+         [
+            'title' => 'Jupiter- T008',
+            'src' => '/lg_hi_macs/images/catalog/lg_hi_macs_galaxy_t008_jupiter.jpg',
+        ],
+        [
+            'title' => 'Nebula- T010',
+            'src' => '/lg_hi_macs/images/catalog/lg_hi_macs_galaxy_t010_nebula.jpg',
         ],
     ],
 ];
