8ad55b5f6e6d90759588a80056fa0d19cb4f6d2b
Vasilii Chugunov <naukomania@gmail.com>
2017-03-03 13:35:21 +0300

    Добавление основных моделей

---
+++ protected/models/Brand.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * This is the model class for table "brand".
+ *
+ * The followings are the available columns in table 'brand':
+ * @property integer $id
+ * @property string $name
+ * @property string $image
+ * @property string $description
+ * @property string $descriptionCollEx
+ * @property integer $meta_id
+ *
+ * The followings are the available model relations:
+ * @property Meta $meta
+ * @property Collection[] $collections
+ */
+class Brand extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'brand';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('name', 'required'),
+			array('meta_id', 'numerical', 'integerOnly'=>true),
+			array('name, image', 'length', 'max'=>255),
+			array('description, descriptionCollEx', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, name, image, description, descriptionCollEx, meta_id', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'meta' => array(self::BELONGS_TO, 'Meta', 'meta_id'),
+			'collections' => array(self::HAS_MANY, 'Collection', 'brand_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'name' => 'Name',
+			'image' => 'Image',
+			'description' => 'Description',
+			'descriptionCollEx' => 'Description Coll Ex',
+			'meta_id' => 'Meta',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('image',$this->image,true);
+		$criteria->compare('description',$this->description,true);
+		$criteria->compare('descriptionCollEx',$this->descriptionCollEx,true);
+		$criteria->compare('meta_id',$this->meta_id);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Brand the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
