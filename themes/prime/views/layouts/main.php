068d457ef0a659966f2d26e87d7d6e870862d4df
Larisa <kosmetichka@mail.ru>
2017-04-10 14:03:18 +0300

    РОбот txt

--- themes/prime/views/layouts/main.php
+++ themes/prime/views/layouts/main.php
@@ -386,5 +386,15 @@
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="<?= Yii::app()->request->baseUrl; ?>/vendor/bootstrap/js/bootstrap.js"></script>
 <script src="<?= Yii::app()->request->baseUrl; ?>/js/script.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-97110571-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
